da3b5b4d32169c05e006e4b48c9427df58cbe0bf
ChandraSekharReddyKarri <129104701+ChandraSekharReddyKarri@users.noreply.github.com>
2024-06-14 00:15:38 +0530

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,10 +12,25 @@ module.exports = {
     publicPath: '/'
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   module: {
     rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ],
+            plugins: ['@babel/plugin-syntax-jsx']
+          }
+        }
+      },
       {
         test: /\.(ts|tsx)$/,
         use: 'babel-loader',
